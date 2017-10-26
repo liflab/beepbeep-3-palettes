b8eccb957d9eeb7e023e4f12e0203f2166cf5821
Sylvain Hallé <shalle@acm.org>
2017-10-26 17:51:53 -0400

    Added method to send POST requests

--- Http/src/ca/uqac/lif/cep/http/HttpListenerTest.java
+++ Http/src/ca/uqac/lif/cep/http/HttpListenerTest.java
@@ -20,12 +20,15 @@ package ca.uqac.lif.cep.http;
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Queue;
 
 import org.junit.Test;
+
+import com.sun.net.ssl.HttpsURLConnection;
 
 import ca.uqac.lif.cep.Connector;
 import ca.uqac.lif.cep.tmf.QueueSink;
@@ -78,4 +81,42 @@ public class HttpListenerTest
 		in.close();
 		return response.toString();
 	}
+	
+	protected static String sendPost() throws Exception 
+	{
+		String url = "https://selfsolve.apple.com/wcResults.do";
+		URL obj = new URL(url);
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+		//add reuqest header
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", "Dummy Agent");
+		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+		String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+		// Send post request
+		con.setDoOutput(true);
+		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		wr.writeBytes(urlParameters);
+		wr.flush();
+		wr.close();
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'POST' request to URL : " + url);
+		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		return response.toString();
+	}
+
 }
