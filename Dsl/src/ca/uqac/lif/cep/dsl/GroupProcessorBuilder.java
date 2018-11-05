e8599fbf9d8d6ab7b1dc035fe0d4bbf38b339df1
Sylvain Hallé <shalle@acm.org>
2018-11-05 10:19:55 +0100

    Small cleanup to Dsl

--- Dsl/src/ca/uqac/lif/cep/dsl/GroupProcessorBuilder.java
+++ Dsl/src/ca/uqac/lif/cep/dsl/GroupProcessorBuilder.java
@@ -29,102 +29,107 @@ import ca.uqac.lif.cep.Processor;
 import ca.uqac.lif.cep.tmf.Fork;
 import ca.uqac.lif.cep.tmf.Passthrough;
 
+/**
+ * A special type of {@link GrammarObjectBuilder} that encapsulates the
+ * resulting chain into a {@link GroupProcessor}.
+ * @author Sylvain Hallé
+ */
 public class GroupProcessorBuilder extends GrammarObjectBuilder<GroupProcessor>
 {
-	protected Set<Processor> m_processors;
-	
-	protected Map<Object,Fork> m_inputForks;
-	
-	public GroupProcessorBuilder()
-	{
-		super();
-		m_processors = new HashSet<Processor>();
-		m_inputForks = new HashMap<Object,Fork>();
-	}
-	
-	public Processor add(Processor ... procs)
-	{
-		Processor last = null;
-		for (Processor p : procs)
-		{
-			m_processors.add(p);
-			last = p;
-		}
-		return last;
-	}
-	
-	public void remove(Processor ... procs)
-	{
-		for (Processor p : procs)
-		{
-			m_processors.remove(p);
-		}
-	}
-	
-	@Override
-	protected synchronized GroupProcessor postVisit(Deque<Object> stack)
-	{
-		if (stack.isEmpty())
-		{
-			return null;
-		}
-		Processor p = (Processor) stack.peek();
-		GroupProcessor gp = new GroupProcessor(m_inputForks.size(), p.getOutputArity());
-		for (Processor in_p : m_processors)
-		{
-			gp.addProcessor(in_p);
-		}
-		int index = 0;
-		for (Map.Entry<Object,Fork> entry : m_inputForks.entrySet())
-		{
-			Fork f = entry.getValue();
-			gp.addProcessor(f);
-			if (f.getPullableInput(0) != null)
-			{
-				// This is an "internal" fork already connected to something inside
-				continue;
-			}
-			gp.associateInput(index, f, 0);
-			index++;
-		}
-		for (int i = 0; i < p.getOutputArity(); i++)
-		{
-			gp.associateOutput(i, p, i);
-		}
-		return gp;
-	}
-	
-	public Fork getFork(Object name)
-	{
-		Fork f = null;
-		if (m_inputForks.containsKey(name))
-		{
-			f = m_inputForks.get(name);
-		}
-		else
-		{
-			f = newFork();
-			m_inputForks.put(name, f);
-		}
-		return f;
-	}
-	
-	protected Fork newFork()
-	{
-		return new Fork(0);
-	}
+  protected Set<Processor> m_processors;
 
-	public Passthrough forkInput(Object name)
-	{
-		Fork f = getFork(name);
-		int f_arity = f.getOutputArity();
-		f.extendOutputArity(f_arity + 1);
-		Passthrough pt = new Passthrough();
-		Connector.connect(f, f_arity, pt, 0);
-		return pt;
-	}
-	
-	/**
+  protected Map<Object,Fork> m_inputForks;
+
+  public GroupProcessorBuilder()
+  {
+    super();
+    m_processors = new HashSet<Processor>();
+    m_inputForks = new HashMap<Object,Fork>();
+  }
+
+  public Processor add(Processor ... procs)
+  {
+    Processor last = null;
+    for (Processor p : procs)
+    {
+      m_processors.add(p);
+      last = p;
+    }
+    return last;
+  }
+
+  public void remove(Processor ... procs)
+  {
+    for (Processor p : procs)
+    {
+      m_processors.remove(p);
+    }
+  }
+
+  @Override
+  protected synchronized GroupProcessor postVisit(Deque<Object> stack)
+  {
+    if (stack.isEmpty())
+    {
+      return null;
+    }
+    Processor p = (Processor) stack.peek();
+    GroupProcessor gp = new GroupProcessor(m_inputForks.size(), p.getOutputArity());
+    for (Processor in_p : m_processors)
+    {
+      gp.addProcessor(in_p);
+    }
+    int index = 0;
+    for (Map.Entry<Object,Fork> entry : m_inputForks.entrySet())
+    {
+      Fork f = entry.getValue();
+      gp.addProcessor(f);
+      if (f.getPullableInput(0) != null)
+      {
+        // This is an "internal" fork already connected to something inside
+        continue;
+      }
+      gp.associateInput(index, f, 0);
+      index++;
+    }
+    for (int i = 0; i < p.getOutputArity(); i++)
+    {
+      gp.associateOutput(i, p, i);
+    }
+    return gp;
+  }
+
+  public Fork getFork(Object name)
+  {
+    Fork f = null;
+    if (m_inputForks.containsKey(name))
+    {
+      f = m_inputForks.get(name);
+    }
+    else
+    {
+      f = newFork();
+      m_inputForks.put(name, f);
+    }
+    return f;
+  }
+
+  protected Fork newFork()
+  {
+    return new Fork(0);
+  }
+
+  public Passthrough forkInput(Object name)
+  {
+    Fork f = getFork(name);
+    int f_arity = f.getOutputArity();
+    f.extendOutputArity(f_arity + 1);
+    Passthrough pt = new Passthrough();
+    Connector.connect(f, f_arity, pt, 0);
+    return pt;
+  }
+
+  /**
    * Encapsulates a processor into a group processor
    * @param p The processor to encapsulate
    * @return A {@link GroupProcessor} containing only p inside
