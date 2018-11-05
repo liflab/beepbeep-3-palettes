a1344f4ec39bd31f09c8c53a0866e0b287e41153
Sylvain Hallé <shalle@acm.org>
2018-11-05 10:19:41 +0100

    Dummy main class to each palette

---
+++ Tuples/src/ca/uqac/lif/cep/tuples/Main.java
@@ -0,0 +1,31 @@
+/*
+    BeepBeep, an event stream processor
+    Copyright (C) 2008-2018 Sylvain Hallé
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ca.uqac.lif.cep.tuples;
+
+/**
+ * Dummy main file
+ * @author Sylvain Hallé
+ */
+public class Main
+{
+  public static void main(String[] args)
+  {
+    System.out.println("This JAR file is not meant to be run from the command line");
+    System.exit(0);
+  }
+}
