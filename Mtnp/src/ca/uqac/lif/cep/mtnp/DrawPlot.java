ba7c6244189359cc7ab4449f77744155d0fc9e1f
Sylvain Hallé <shalle@acm.org>
2026-02-08 09:45:28 -0500

    DrawPlot accepts Maps in addition to Tables

--- Mtnp/src/ca/uqac/lif/cep/mtnp/DrawPlot.java
+++ Mtnp/src/ca/uqac/lif/cep/mtnp/DrawPlot.java
@@ -1,6 +1,6 @@
 /*
     BeepBeep, an event stream processor
-    Copyright (C) 2008-2017 Sylvain Hallé
+    Copyright (C) 2008-2026 Sylvain Hallé
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published
@@ -17,12 +17,17 @@
  */
 package ca.uqac.lif.cep.mtnp;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import ca.uqac.lif.cep.Processor;
 import ca.uqac.lif.cep.ProcessorException;
 import ca.uqac.lif.cep.UniformProcessor;
 import ca.uqac.lif.mtnp.plot.Plot;
 import ca.uqac.lif.mtnp.plot.Plot.ImageType;
+import ca.uqac.lif.mtnp.table.HardTable;
 import ca.uqac.lif.mtnp.table.Table;
+import ca.uqac.lif.mtnp.table.TableEntry;
 
 /**
  * Generates a bitmap from a {@link ca.uqac.lif.mtnp.table.Table Table}
@@ -75,7 +80,7 @@ public class DrawPlot extends UniformProcessor
 	@Override
 	protected boolean compute(Object[] inputs, Object[] outputs) throws ProcessorException 
 	{
-		Table t = (Table) inputs[0];
+		Table t = getTable(inputs[0]);
 		m_plot.setTable(t);
 		byte[] bytes = m_plot.getImage(m_type);
 		outputs[0] = bytes;
@@ -87,5 +92,37 @@ public class DrawPlot extends UniformProcessor
 	{
 		// Does not make much sense to clone this
 		throw new UnsupportedOperationException();
+	}
+	
+	/**
+	 * Obtains a {@link Table} object from an input. The method accepts
+	 * either a table, or a {@link Map} that is converted into a two-column
+	 * table.
+	 * @param o The input object
+	 * @return A table instance
+	 * @since 3.14
+	 */
+	@SuppressWarnings("rawtypes")
+	protected static Table getTable(Object o)
+	{
+		if (o instanceof Table)
+		{
+			return (Table) o;
+		}
+		if (o instanceof Map)
+		{
+			// Convenience method: turns a map into a table
+			HardTable t = new HardTable("x", "y");
+			for (Object o_e : ((Map) o).entrySet())
+			{
+				Entry<?,?> e = (Entry<?,?>) o_e;
+				TableEntry te = new TableEntry();
+				te.put("x", e.getKey());
+				te.put("y", e.getValue());
+				t.add(te);
+			}
+			return t;
+		}
+		throw new IllegalArgumentException("Expected a table or a map");
 	}
 }
